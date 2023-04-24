59d7540fab52a13451635b3cbe9b9465af9f565b
Vania Pereira <vania-pereira.94@hotmail.com>
2023-04-24 22:36:49 +0100

    Logótipo e level3controller

---
+++ Assets/Level3Controller.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+/*
+ * Controla o nível 3.
+ * O nível consiste em empurrar o adversário para fora do ringue.
+ * O nível é constituido por várias rondas.
+*/
+
+public class Level3Controller : MonoBehaviour
+{
+
+    /* ATRIBUTOS PRIVADOS */
+
+    // variável para a referência do controlador de jogo
+    private GameController _game;
+
+    // variáveis para guardar os jogadores do nível
+    private GameObject _player1Object;
+    private GameObject _player2Object;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _game = GameController.Instance;
+
+        // TEST: usar isto enquanto é testado apenas o nível atual (sem iniciar pelo menu)
+        _game.Players = new List<PlayerModel>();
+        _game.InitiateGame();
+
+
+
+        // exibir objetos na cena
+        SpawnPlayer1();
+        SpawnPlayer2();
+
+
+
+    }
+    void SpawnPlayer1()
+    {
+        _player1Object = Instantiate(_game.Players[0].prefab);
+    }
+
+    void SpawnPlayer2()
+    {
+        _player2Object = Instantiate(_game.Players[1].prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
